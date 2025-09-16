10f8b1a9091b1e3a921e6e652322e3765a52a53b
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-09-16 19:17:41 -0400

    pt2

---
+++ files.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Files</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <h1>Welcome to My PHP Page</h1>
+    <p>This is a simple PHP page embedded in HTML.</p>
+
+    <?php
+        // PHP code goes here
+        echo "Files download as pdfs";
+    ?>
+    <a href="/[...]" download><img [...]></a>
+</body>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> My PHP Page</p>
+    </footer>
+</html>
