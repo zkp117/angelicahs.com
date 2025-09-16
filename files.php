fb9f5a471e8b333e0fd3e414bcda5800b92852a2
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-09-16 19:19:24 -0400

    pt.3

--- files.php
+++ files.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title>Files</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
@@ -16,4 +17,5 @@
     <footer>
         <p>&copy; <?php echo date("Y"); ?> My PHP Page</p>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </html>
